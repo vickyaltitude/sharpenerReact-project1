042382bf2a7c44d5b442ca7ab75d26362220f21c
vickyaltitude <vignvick3005>
2024-12-07 19:55:43 +0530

    added variable instead of hardcode values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+    let showDate = new Date().toISOString()
+    let location = 'Delhi';
+    let product = 'Book';
+    let price = 10;
   return (
     <div className='expense-item'>
-      <div>15 August 2023</div>
-      <div className='expense-item__location'>Delhi</div>
+      <div>{showDate}</div>
+      <div className='expense-item__location'>{location}</div>
       <div className='expense-item__description'>
-        <h2>Book</h2>
-        <div className='expense-item__price'>$10</div>
+        <h2>{product}</h2>
+        <div className='expense-item__price'>${price}</div>
       </div>
     </div>
   );
