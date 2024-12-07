a3b8853909c03f81efa44b53b5973e2347b9f02a
vickyaltitude <vignvick3005>
2024-12-07 19:37:37 +0530

    create project1 by following sharpener assignments

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+    <h1>Let's get Started</h1>
+    <ExpenseItem />
+  </div>
+  );
+}
+
+export default App;
