de6661b5d8e7a9c69b703a506d4062134d493b16
vickyaltitude <vignvick3005>
2024-12-07 21:08:51 +0530

    added calender like look for date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
+// Write your code here
 
 import "./ExpenseItem.css";
 
+let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
 function ExpenseItem(props) {
-  
+    let month = months[props.date.getMonth()]
+    let year = props.date.getFullYear();
+    let dates = props.date.getDate();
   return (
     <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+          <div>
+              <div>{month}</div>
+              <div>{year}</div>
+              <div>{dates}</div>
+          </div>
     <div className='expense-item__location'>{props.location}</div>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
