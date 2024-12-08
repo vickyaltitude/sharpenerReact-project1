46fee85e7c1d2b8d9269786a54dcc1a1c60a28ee
vickyaltitude <vignvick3005>
2024-12-08 15:29:02 +0530

    added form handler function to submit form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -37,22 +37,31 @@ const ExpenseForm = ()=>{
           }
         })
       };
-    
+     
+      function formSubmitHandler(e){
+        e.preventDefault();
+        console.log(userInput);
+        setUserInput({
+            enteredTitle: "",
+            enteredAmount: "",
+            enteredDate:""
+          })
+      }
      
    return (
-<form>
-        <div className='new-expense__controls'>
+<form onSubmit={formSubmitHandler}>
+        <div className='new-expense__controls' >
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler}/>
+                <input type='text' value={userInput.enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' onChange={amountChangeHandler}/>
+                <input type='number' value={userInput.enteredAmount} onChange={amountChangeHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min="2023-01-01" max="2025-12-31" onChange={dateChangeHandler}/>
+                <input type='date' min="2023-01-01" value={userInput.enteredDate} max="2025-12-31" onChange={dateChangeHandler}/>
             </div>
             <div className="new-expense__actions">
                 <button type='submit'>Add Expense</button>
